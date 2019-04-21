b85071e7a9119384cdc7b4315fcc7728098eea17
lucas2502 <lucas2502@live.com>
2019-04-21 10:35:10 -0300

    Create controller for authentication

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowecase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false 
+    },
+    CreatedAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
